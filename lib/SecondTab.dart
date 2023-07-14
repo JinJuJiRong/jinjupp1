a5e9283204b96887036f96de8808fecf6c1a53a9
황진주 <vjinjoov@naver.com>
2023-07-14 10:38:29 +0900

    각 브랜치생성

---
+++ lib/SecondTab.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'FirstTab.dart';
+import 'main.dart';
+import 'ThirdTab.dart';
+
+// 두번째 페이지
+class SecondTab extends StatelessWidget {
+  const SecondTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('두번째 페이지'));
+  }
+}
---
+++ lib/ThirdTab.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'FirstTab.dart';
+import 'SecondTab.dart';
+import 'main.dart';
+
+// 세번째 페이지
+class ThirdTab extends StatelessWidget {
+  const ThirdTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const chartData = {
+      'korea': [
+        {
+          'imageUrl': 'https://i.ibb.co/xf2HpfG/dynamite.jpg',
+          'name': 'Dynamite',
+          'artist': 'BTS',
+        },
+        {
+          'imageUrl': 'https://i.ibb.co/xf2HpfG/dynamite.jpg',
+          'name': 'Dynamite',
+          'artist': 'BTS',
+        },
+        {
+          'imageUrl': 'https://i.ibb.co/xf2HpfG/dynamite.jpg',
+          'name': 'Dynamite',
+          'artist': 'BTS',
+        },
+      ],
+      'global': [
+        {
+          'imageUrl': 'https://i.ibb.co/xf2HpfG/dynamite.jpg',
+          'name': 'Dynamite',
+          'artist': 'BTS',
+        },
+        {
+          'imageUrl': 'https://i.ibb.co/xf2HpfG/dynamite.jpg',
+          'name': 'Dynamite',
+          'artist': 'BTS',
+        },
+        {
+          'imageUrl': 'https://i.ibb.co/xf2HpfG/dynamite.jpg',
+          'name': 'Dynamite',
+          'artist': 'BTS',
+        },
+      ],
+      'newyork': [
+        {
+          'imageUrl': 'https://i.ibb.co/xf2HpfG/dynamite.jpg',
+          'name': 'Dynamite',
+          'artist': 'BTS',
+        },
+        {
+          'imageUrl': 'https://i.ibb.co/xf2HpfG/dynamite.jpg',
+          'name': 'Dynamite',
+          'artist': 'BTS',
+        },
+        {
+          'imageUrl': 'https://i.ibb.co/xf2HpfG/dynamite.jpg',
+          'name': 'Dynamite',
+          'artist': 'BTS',
+        },
+      ],
+    };
+
+    return Center(child: Text('세번째 페이지'));
+  }
+}
